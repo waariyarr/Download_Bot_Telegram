00ea249a7a8b14402e19dbf7ea9e517c19a60e99
waariyarr <raj.kumar2024@lpu.in>
2025-12-14 15:53:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 # config.py
 
 # Get these from https://my.telegram.org
-API_ID = 24252399                 # Replace with your integer ID
-API_HASH = "a1671d698b4fba4ad05c918d6db601d4"   # Replace with your string Hash
-BOT_TOKEN = "8281831703:AAErQzOZKZS6N6VuegfdtUZKVFGL8FltWPk" # Replace with your Bot Token
+API_ID = 12345678                 # Replace with your integer ID
+API_HASH = ""   # Replace with your string Hash
+BOT_TOKEN = "" # Replace with your Bot Token
 
 # Other settings
+
 DOWNLOAD_LOCATION = "./downloads"
